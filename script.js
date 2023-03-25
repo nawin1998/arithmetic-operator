d4e2093b75f24770aa32d7dbc9e9327f4cab1b0a
nawin1998 <120089267+nawin1998@users.noreply.github.com>
2023-03-25 10:46:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+function add(){
+    let submit1= Number(document.getElementById("submit1").value);
+    let submit2= Number(document.getElementById("submit2").value);
+    let add=submit1+submit2;
+    alert(add);
+}
+function sub(){
+    let submit1= Number(document.getElementById("submit1").value);
+    let submit2= Number(document.getElementById("submit2").value);
+    let sub=submit1-submit2;
+    alert(sub);
+}
+function mult(){
+    let submit1= Number(document.getElementById("submit1").value);
+    let submit2= Number(document.getElementById("submit2").value);
+    let mult=submit1*submit2;
+    alert(mult);
+}
+function div(){
+    let submit1= Number(document.getElementById("submit1").value);
+    let submit2= Number(document.getElementById("submit1").value);
+    let div=submit1/submit2;
+    alert(div);
+}
+function mod(){
+    let submit1= Number(document.getElementById("submit1").value);
+    let submit2= Number(document.getElementById("submit2").value);
+    let mod=submit1%submit2;
+    alert(mod);
+}
